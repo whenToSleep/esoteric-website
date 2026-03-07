d0bfdf75973b3ccea5a64f88e5c5745816718b4a
whenToSleep <jenyakoychev@gmail.com>
2026-03-07 02:53:26 +0000

    migration

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260307_024232 from './20260307_024232';
+import * as migration_20260307_025052 from './20260307_025052';
 
 export const migrations = [
   {
     up: migration_20260307_024232.up,
     down: migration_20260307_024232.down,
-    name: '20260307_024232'
+    name: '20260307_024232',
+  },
+  {
+    up: migration_20260307_025052.up,
+    down: migration_20260307_025052.down,
+    name: '20260307_025052'
   },
 ];
